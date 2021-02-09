c4b70a0de43d5a4c76366429ec3ddf2de017d463
heymiriam <miriambellon@gmail.com>
2021-02-09 12:42:47 -0800

    shopping cart popup

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const cart = document.querySelector('.cart');
-const popup = document.querySelector('.popup');
- 
-cart.addEventListener('click', () => {
-    popup.style.display = 'block';
+window.onload=function(){
+let openPopup = document.getElementById('btn-open-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	closePopup = document.getElementById('btn-close-popup');
+
+    openPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
 });
+
+closePopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
+}
