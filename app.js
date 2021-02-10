9e2219efc2376a38956efe9f7e1d63251efc5173
heymiriam <miriambellon@gmail.com>
2021-02-09 21:36:27 -0800

    caroussel for brands

--- app.js
+++ app.js
@@ -33,23 +33,47 @@ closePops.addEventListener('mouseout', function(e){
 	submenu.classList.remove('active');
 	pops.classList.remove('active');
 });
+
+
+
+
+
 }
 
-/*SUBMENU*/
-/*window.onload=function(){
-let openPops = document.getElementById('open-popup'),
-	submenu = document.getElementById('submenu'),
-	pops = document.getElementById('pops'),
-	closePops= document.getElementById('close-popup');
+/*CAROUSSEL*/ 
+let slideIndex = 1;
+showSlides(slideIndex);
 
-    openPops.addEventListener('click', function(){
-	submenu.classList.add('active');
-	pops.classList.add('active');
-});
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-closePops.addEventListener('click', function(e){
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+/*closePopup.addEventListener('click', function(e){
 	e.preventDefault();
-	submenu.classList.remove('active');
-	pops.classList.remove('active');
-});
-}*/
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});*/
