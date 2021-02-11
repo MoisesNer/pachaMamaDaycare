4da5f44386b350760a48b885dc99b7aadbd556b4
Moises Nervis <Nervis@Macs-iMac-2.local>
2021-02-11 12:47:31 -0800

    cart working, needs formating

--- app.js
+++ app.js
@@ -74,26 +74,73 @@ slides[slideIndex-1].style.display = "block";
 }
 
 //ALERT SHOPPING CART
-const addCart = document.getElementById('AddCart');
-const qty = document.getElementById('qty');
+// const addCart = document.getElementById('AddCart');
+const qty = document.querySelectorAll('qty');
 
-qty.addEventListener('keypress', function(e){
-  if(e.key === 'Enter'){
-    alert(`You have added ${this.value} item(s) to your shopping cart`);
-  }
-  e.preventDefault()
+qty.forEach(function (enter){
+  enter.addEventListener('keypress', function(e){
+    if(e.key === 'Enter'){
+      
+      e.preventDefault()
+    }
+  })
 });
 
-addCart.addEventListener('click', function(){
-  alert(`You have added ${qty.value} item(s) to your shopping cart`);
+// qty.addEventListener('keypress', function(e){
+//   if(e.key === 'Enter'){
+    
+//     e.preventDefault()
+    // alert(`You have added ${this.value} item(s) to your shopping cart`);
+//   }
+// });
 
-});
+// addCart.addEventListener('click', function(){
+//   alert(`You have added ${qty.value} item(s) to your shopping cart`);
 
+// });
 
+// COLOR CIRCLE
+// let color = document.querySelector(".circle");
+// color.addEventListener('click', function(){
+//   document.color.style.border = '2px solid black';
+// });
+// color.addEventListener("click", black);
 
-let color = document.querySelector(".circle");
+//adding to cart
+const addToCart = document.querySelectorAll('.add-cart');
 
-color.addEventListener('click', function(){
-  document.color.style.border = '2px solid black';
-});
-color.addEventListener("click", black);
+addToCart.forEach(function(btn){
+
+  btn.addEventListener('click', function(event){
+    let name = event.target.parentElement.parentElement.parentElement.parentElement.firstChild.nextSibling.firstChild.nextSibling.innerText;
+    let price = event.target.parentElement.parentElement.parentElement.firstChild.nextSibling.textContent;
+    let quantity = parseInt(event.target.parentElement.parentElement.parentElement.firstChild.nextSibling.nextSibling.nextSibling.firstChild.nextSibling.value);
+    
+    const item = {};
+    item.name = name;
+    item.price = price.slice(1).trim();
+    item.quantity = quantity;
+
+    console.log(item);
+    const cartItem = document.createElement('div')
+    cartItem.classList.add('cartItem');
+
+    cartItem.innerHTML =
+
+    // `
+    // <div class='cartItem'>
+      //  <h5 id="cartPrice" class="cartPrice">$ 40.00</h5>
+      //  <h5 id="nameItem" class="nameItem">Product Name</h5>
+      // <h6 id="qtyItem" class="qtyItem">6 items</h6>
+    // <div >
+    // `
+    `
+      <h5 id="cartPrice" class="cartPrice">$ ${item.price}</h5>
+      <h5 id="nameItem" class="nameItem">${item.name}</h5>
+      <h6 id="qtyItem" class="qtyItem">${item.quantity}</h6>
+    `
+    // SELECT CART
+    popup = document.getElementById('popup')
+    popup.appendChild(cartItem);
+  })
+})
