2040e62948c6fe3abb99d7b21418f15df2b59ff9
Moises Nervis <StarCommand@Moisess-iMac.local>
2021-02-11 23:38:54 -0800

    carrito con estilos, creando total amount - suma en el carrito

--- app.js
+++ app.js
@@ -1,141 +1,97 @@
-// comment
 /*SHOPPING-CART*/
 window.onload=function(){
-
-    let openPopup = document.getElementById('btn-open-popup'),
-        overlay = document.getElementById('overlay'),
-        popup = document.getElementById('popup'),
-        closePopup = document.getElementById('btn-close-popup');
-    
-        openPopup.addEventListener('click', function(){
-        overlay.classList.add('active');
-        popup.classList.add('active');
-    });
-    
-    closePopup.addEventListener('click', function(e){
-        e.preventDefault();
-        overlay.classList.remove('active');
-        popup.classList.remove('active');
-    });
-    
-    
-    
-    let openPops = document.getElementById('open-popup'),
-        submenu = document.getElementById('submenu'),
-        pops = document.getElementById('pops'),
-        closePops= document.getElementById('close-popup'),
-        openPopsMobile = document.getElementById('open-popup-mobile'),
-        closePopsMobile = document.getElementById('close-popup-mobile');
-        openPops.addEventListener('mouseover', function(){
-        submenu.classList.add('active');
-        pops.classList.add('active');
-    });
-    
-    closePops.addEventListener('mouseout', function(e){
-        e.preventDefault();
-        submenu.classList.remove('active');
-        pops.classList.remove('active');
-    });
-    
-    /*CLICK ON PRODUCT FOR MOBILE AND TABLE*/
-    
-    openPopsMobile.addEventListener('click', function(e){
-        e.preventDefault();
-        submenu.classList.add('active');
-        pops.classList.add('active');
-    });
-    closePopsMobile.addEventListener('click', function(e){
-        e.preventDefault();
-        submenu.classList.remove('active');
-        pops.classList.remove('active');
-    });
-        
+  let openPopup = document.getElementById('btn-open-popup'),
+      overlay = document.getElementById('overlay'),
+      popup = document.getElementById('popup'),
+      closePopup = document.getElementById('btn-close-popup');
+  
+      openPopup.addEventListener('click', function(){
+      overlay.classList.add('active');
+      popup.classList.add('active');
+  });
+  
+  closePopup.addEventListener('click', function(e){
+      e.preventDefault();
+      overlay.classList.remove('active');
+      popup.classList.remove('active');
+  });
+  
+  
+  
+  let openPops = document.getElementById('open-popup'),
+      submenu = document.getElementById('submenu'),
+      pops = document.getElementById('pops'),
+      closePops= document.getElementById('close-popup'),
+      openPopsMobile = document.getElementById('open-popup-mobile'),
+      closePopsMobile = document.getElementById('close-popup-mobile');
+      openPops.addEventListener('mouseover', function(){
+      submenu.classList.add('active');
+      pops.classList.add('active');
+  });
+  
+  closePops.addEventListener('mouseout', function(e){
+      e.preventDefault();
+      submenu.classList.remove('active');
+      pops.classList.remove('active');
+  });
+  
+  /*CLICK ON PRODUCT FOR MOBILE AND TABLE*/
+  
+  openPopsMobile.addEventListener('click', function(e){
+      e.preventDefault();
+      submenu.classList.add('active');
+      pops.classList.add('active');
+  });
+  closePopsMobile.addEventListener('click', function(e){
+      e.preventDefault();
+      submenu.classList.remove('active');
+      pops.classList.remove('active');
+  });
+ 
+  }
+  
+  /*CAROUSSEL*/ 
+  let slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    
-    /*CAROUSSEL*/ 
-    let slideIndex = 1;
-    showSlides(slideIndex);
-    
-    // Next/previous controls
-    function plusSlides(n) {
-      showSlides(slideIndex += n);
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
     }
-    
-    // Thumbnail image controls
-    function currentSlide(n) {
-      showSlides(slideIndex = n);
-    }
-    
-    function showSlides(n) {
-      let i;
-      let slides = document.getElementsByClassName("mySlides");
-      let dots = document.getElementsByClassName("dot");
-      if (n > slides.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";
-      }
-      for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "");
-      }
-    
-    slides[slideIndex-1].style.display = "block";
-    
-      dots[slideIndex-1].className += " active";
-    }
+  
+  slides[slideIndex-1].style.display = "block";
+  
+    dots[slideIndex-1].className += " active";
+  }
 
 //COLORDOT FUNCTION
 
-let colorDot = document.querySelector(".circle");
+// let colorDot = document.querySelector(".circle");
     
-    colorDot.addEventListener('click', function(){
-      document.colorDot.style.border = '2px solid black';
-    });
-    color.addEventListener("click", black);
+//     colorDot.addEventListener('click', function(){
+//       document.colorDot.style.border = '2px solid black';
+//     });
+//     color.addEventListener("click", black);
 
-        //ALERT SHOPPING CART
-    const addCart = document.getElementById('AddCart');
-    const qty = document.getElementById('qty');
-    
-    qty.addEventListener('keypress', function(e){
-      if(e.key === 'Enter'){
-        alert(`You have added ${this.value} item(s) to your shopping cart`);
-      }
-      e.preventDefault()
-    });
-    
-    addCart.addEventListener('click', function(){
-      alert(`You have added ${qty.value} item(s) to your shopping cart`);
-    
-    });
-    
-    //ALERT SHOPPING CART
-// const addCart = document.getElementById('AddCart');
-const qty = document.querySelectorAll('qty');
-
-qty.forEach(function (enter){
-  enter.addEventListener('keypress', function(e){
-    if(e.key === 'Enter'){
-      
-      e.preventDefault()
-    }
-  })
-});
-
-// qty.addEventListener('keypress', function(e){
-//   if(e.key === 'Enter'){
-    
-//     e.preventDefault()
-    // alert(`You have added ${this.value} item(s) to your shopping cart`);
-//   }
-// });
-
-// addCart.addEventListener('click', function(){
-//   alert(`You have added ${qty.value} item(s) to your shopping cart`);
-
-// });
-
-//ADDING TO CART
+ //ADDING TO CART
 const addToCart = document.querySelectorAll('.add-cart');
 
 addToCart.forEach(function(btn){
@@ -150,26 +106,41 @@ addToCart.forEach(function(btn){
     item.price = price.slice(1).trim();
     item.quantity = quantity;
 
-    console.log(item);
     const cartItem = document.createElement('div')
     cartItem.classList.add('cartItem');
 
     cartItem.innerHTML =
 
-    // `
-    // <div class='cartItem'>
-      //  <h5 id="cartPrice" class="cartPrice">$ 40.00</h5>
-      //  <h5 id="nameItem" class="nameItem">Product Name</h5>
-      // <h6 id="qtyItem" class="qtyItem">6 items</h6>
-    // <div >
-    // `
     `
-      <h5 id="cartPrice" class="cartPrice">$ ${item.price}</h5>
-      <h5 id="nameItem" class="nameItem">${item.name}</h5>
-      <h6 id="qtyItem" class="qtyItem">${item.quantity}</h6>
+    <p class="pItem">${item.quantity} item(s)</p>
+    <p>${item.name}</p>
+    <p>$ ${item.price}</p>                      
+    <a href="#" id='cart-item-remove' class="cart-item-remove">
+      <i class='fas fa-trash'></i>
+    </a>
     `
     //PRINTING INTO CART
     popup = document.getElementById('popup')
     popup.appendChild(cartItem);
+    
+    alert('item added to the cart');
+    
+    
+    showTotal();
   })
-})
+
+  function showTotal(){
+    const total = [];
+    const items = document.querySelectorAll('.price-item');
+
+    items.forEach(function(item){
+      total.push(parseFloat(item.textContent.slice(1).trim()))//getting the amount
+    });
+    
+    const totalMoney = total.reduce(function(total, item){
+      total += item;
+      return total;
+    }, 0);
+    console.log(totalMoney);
+  };
+});
